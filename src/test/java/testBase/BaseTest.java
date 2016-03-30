a723d8696da3d98b9de1a827a1006e8f126d13ae
Hamid <islam.md1971@gmail.com>
2016-03-29 23:01:17 -0400

    Created page element

--- src/test/java/testBase/BaseTest.java
+++ src/test/java/testBase/BaseTest.java
@@ -1,9 +1,17 @@
 package testBase;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
 	
 	WebDriver driver = null;
+	
+	@BeforeMethod
+	public void start(){
+		
+		driver = new FirefoxDriver();
+	}
 
 }
