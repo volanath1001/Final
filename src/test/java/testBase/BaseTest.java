70cdfb4bd2f7ebbb84540fe33a4928769640007e
= <=>
2016-03-30 23:55:27 -0400

    shloud conflict from Masud

--- src/test/java/testBase/BaseTest.java
+++ src/test/java/testBase/BaseTest.java
@@ -25,6 +25,7 @@ public class BaseTest {
 		
 		System.out.println("HYYYYYYYYYYYY");
 
+		System.out.println("Should conflict");
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 	}
 
