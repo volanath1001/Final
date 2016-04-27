9a5cd12212285cbbe04ba6c7b43c68ffac12cbef
Hamid <islam.md1971@gmail.com>
2016-04-26 22:27:13 -0400

    fasdv

--- src/test/java/test/LogInTest.java
+++ src/test/java/test/LogInTest.java
@@ -1,11 +1,9 @@
 package test;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 import org.testng.annotations.Test;
 
-import com.google.common.base.Predicate;
 
 import testBase.BaseTest;
 
